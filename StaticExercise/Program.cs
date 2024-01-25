7d95a2d095cc682b29946ae037155a27bfb2d164
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-25 15:05:54 -0600

    Tempconverter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What temperature is:");
+            double temp = double.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------");
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("What temperature scale is it in? Fahrenheit or Celsius:");
+                string scale = Console.ReadLine().ToLower();
+                switch (scale)
+                {
+                    case "fahrenheit":
+                        Console.WriteLine($"The temperature would be {TempConverter.FahrenheitToCelsius(temp)}°C in Celsius.");
+                        loop = false;
+                        break;
+                    case "f":
+                        Console.WriteLine($"The temperature would be {TempConverter.FahrenheitToCelsius(temp)}°C in Celsius.");
+                        loop = false;
+                        break;
+                    case "celsius":
+                        Console.WriteLine($"The temperature would be {TempConverter.CelsiusToFahrenheit(temp)}°F in Fahrenheit.");
+                        loop = false;
+                        break;
+                    case "c":
+                        Console.WriteLine($"The temperature would be {TempConverter.CelsiusToFahrenheit(temp)}°F in Fahrenheit.");
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("That is not a correct temperature scale.");
+                        break;
+                }
+            }
         }
     }
 }
